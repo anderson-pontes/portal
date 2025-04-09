054fbcb966ae00e21b8b4d58c47d6bf9fbf19971
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-04-09 10:28:02 -0300

    ajuste de rota busca doe

--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -34,7 +34,7 @@ export function Home() {
             title: "BUSCA DOE",
             description: "Sistema de Busca Avançado de Diários Oficiais do Estado do Pará",
             image: imgBuscaDoe,
-            externalLink: "http://buscadoe.pge.pa.gov.br/",
+            externalLink: "/buscadoe/",
         },
         {
             title: "SUPORTE DTI",
